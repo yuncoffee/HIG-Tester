8714d23ce89fda242d4fbead6f920067c485a550
yuncoffee <dong072815@gmail.com>
2023-06-18 21:56:11 +0900

    Initial Commit

---
+++ HIG-Tester/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  HIG-Tester
+//
+//  Created by Yun Dongbeom on 2023/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HIG-Tester/HIG_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIG_TesterApp.swift
+//  HIG-Tester
+//
+//  Created by Yun Dongbeom on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct HIG_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
